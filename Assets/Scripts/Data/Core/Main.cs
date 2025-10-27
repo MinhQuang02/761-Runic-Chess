de9f0038425c187bbc924dcd9781f49674375e45
Quang Minh <139391994+MinhQuang02@users.noreply.github.com>
2025-10-27 16:06:51 +0700

    Committed

--- Assets/Scripts/Data/Core/Main.cs
+++ Assets/Scripts/Data/Core/Main.cs
@@ -1,10 +1,24 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Data.Core
 {
+    /// <summary>
+    /// Lớp chính để chạy chương trình hehehehe
+    /// </summary>
     public static class Main
-    {
+    {   
+        /// <summary>
+        /// Xin chào thế giới
+        /// </summary>
+        public static int test = 5;
 
+        public static int b = 10;
+
+        public static void HelloWorld()
+        {
+            Debug.Log(test);
+            Debug.Log(b);
+        }
     }
 
 }
