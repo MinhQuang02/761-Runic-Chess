01480eb024f2388784e65f2ecef47e15fc5720ba
Quang Minh <139391994+MinhQuang02@users.noreply.github.com>
2025-10-28 21:36:36 +0700

    Commited

---
+++ Assets/Scripts/Data/Core/MainData.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+namespace Data.Core
+{   
+    public static class MainData
+    {
+        // Dữ liệu mặc định của người chơi (phiên chơi bắt đầu từ -1)
+        public static int currentPlayerSession = -1;
+        public static PlayerSetting defaultPlayerSetting = new PlayerSetting();
+
+        // Dữ liệu ánh xạ với từng phiên chơi của người chơi
+        public static string[] playerSessions = new string[3]; // không có dữ liệu của người chơi -1
+        public static bool[] isNewGame = new bool[3] { true, true, true };
+
+        public static PlayerReccord[][] playerReccords = new PlayerReccord[3][]
+        {
+            new PlayerReccord[10],
+            new PlayerReccord[10],
+            new PlayerReccord[10]
+        };
+
+        public static PlayerSetting[] playerSettings = new PlayerSetting[3]
+        {
+            new PlayerSetting(),
+            new PlayerSetting(),
+            new PlayerSetting()
+        };
+
+    }
+
+
+    public struct PlayerReccord
+    {
+        public int numberOfFlags;
+        public int[] flags;
+        public int penalty;
+
+        public PlayerReccord(int init = 0)
+        {
+            numberOfFlags = 0;
+            flags = new int[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            penalty = 0;
+        }
+    }
+
+    public struct PlayerSetting
+    {
+        public bool displayMode;
+        public int resolution;
+        public bool vsync;
+        public int mainVolume;
+        public int musicVolume;
+        public int clickSound;
+        public int hoverSound;
+
+        public PlayerSetting(int init = 0)
+        {
+            displayMode = true; // true là fullscreen, false là windowed
+            resolution = 2; // 0 là Low, 1 là Medium, 2 là High, 3 là Ultra
+            vsync = true; // true là bật, false là tắt
+
+            // âm lượng mặc định là 70
+            mainVolume = 70; 
+            musicVolume = 70;
+            clickSound = 70;
+            hoverSound = 70;
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/MainMenu/RenameButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Data.Core;
+using TMPro;
+
+namespace Objects.MainMenu
+{
+    public class RenameButton : MonoBehaviour
+    {
+        public int buttonIndex;
+        public TextMeshProUGUI[] sessionName = new TextMeshProUGUI [3];
+
+        public void clickRenameButton()
+        {
+            MainData.currentPlayerSession = buttonIndex;
+
+            // Đổi màu chữ của các chữ trở lại màu mặc định và màu của chữ của phiên chơi hiện tại thành màu đặc biệt
+            for (int i = 0; i < 3; i++)
+            {
+                if (i == MainData.currentPlayerSession)
+                {
+                    // Màu xanh đậm đặc biệt
+                    sessionName[i].color = new Color32(87, 88, 95, 200);
+                }
+                else
+                {
+                    sessionName[i].color = new Color32(223, 228, 246, 255); // Màu trắng mặc định
+                }
+            }
+        }
+    }
+}
